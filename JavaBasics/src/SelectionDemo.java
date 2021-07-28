738f963a8dffe23e8691e7ced1aedba25d0d7ed9
britto <britto@192.168.1.7>
2021-07-28 14:08:50 -0400

    Uploading all files

---
+++ JavaBasics/src/SelectionDemo.java
@@ -0,0 +1,23 @@
+
+public class SelectionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 8;
+		
+		int j= 0;
+		
+		/*if(i>6)
+			j=1;
+			
+		else
+			j=2;*/
+		
+		j=i>6?1:2;
+		
+		System.out.println(j);
+		
+	}
+
+}
